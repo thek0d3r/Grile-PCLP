1d589b255127b331ffe442f3ad55ca5d10a81803
thek0der <horjarazvan@gmail.com>
2024-01-13 14:01:01 +0200

    Removed caching

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-    });
-}
