d1ae2c788046a9c728fe7d7338bddfc6a33da44c
thek0d3r <horjarazvan@gmail.com>
2024-01-17 00:24:49 +0200

    Moved ProgressBar to its own component

---
+++ src/routes/ProgressBar.svelte
@@ -0,0 +1,10 @@
+<script>
+
+export let progressPercentage;
+
+</script>
+
+<div class="progress">
+    <div class="progress-bar" role="progressbar" style="width: {progressPercentage}%;" aria-valuenow="{progressPercentage}" aria-valuemin="0" aria-valuemax="100">{Math.floor(progressPercentage)}%</div>
+</div>
+
